e2d8b4f603374e81039710e333d89201f1eaeeed
jaybeedev <jaybeedev@jaybeedev-ubuntu-18.04>
2019-07-30 16:51:37 +0800

    Physician Models Add and Update landing branches UI

--- app/branches/models.py
+++ app/branches/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Physician(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    contact_num = models.IntegerField()
+    
+    is_active = models.BooleanField(default=True)
+    date_join = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
