436ee30bae4fd858d108f28227d4324adc537e76
jaybeedev <jaybeedev@jaybeedev-ubuntu-18.04>
2019-08-15 16:52:46 +0800

    Add list display, list display links, list filter, list editable, search fields, list per page Physician and Schedule Model

--- app/branches/models.py
+++ app/branches/models.py
@@ -7,13 +7,14 @@ class Specialization(models.Model):
     specialty = models.CharField(max_length=60)
     desc = models.TextField(max_length=120)
 
-
+    def __str__(self):
+        return self.specialty
 
 class Clinic(models.Model):
     branch_name  =     models.CharField(max_length=30)
     address =          models.TextField(max_length=120)
 
-    
-    
+    def __str__(self):
+        return self.branch_name
 
     
